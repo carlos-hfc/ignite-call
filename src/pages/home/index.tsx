49ab891abed96b4622366e7070c590f10a0d11ed
Carlos Faustino <chfcchfc96@gmail.com>
2024-04-10 14:07:46 -0300

    feat: styling home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from "@carlos-hfc-ignite-ui/react"
+import Image from "next/image"
+
+import appPreviewImg from "@/assets/app-preview.png"
+
+import { Container, Hero, Preview } from "./styles"
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={appPreviewImg}
+          alt="Calendário simbolizando aplicação"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,2 @@
-import { Heading } from "@carlos-hfc-ignite-ui/react"
+export { default } from "./home";
 
-export default function Home() {
-  return <Heading as="h1">Hello World</Heading>
-}
